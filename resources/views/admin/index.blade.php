54d9588b645209791b6a743e0a105d142735ce9c
Aqib <aqibr777@gmail.com>
2022-08-16 20:24:04 +0500

    004-Create-admin-layout

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,4 @@
+<x-admin-layout>
+    <h1>Admin index file</h1>
+</x-admin-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +11,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
 
 require __DIR__.'/auth.php';
